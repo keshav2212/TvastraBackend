41b18575c0556537897e52d1e154cb5b7cb3e67e
Dakshta Tomar <dakshta.tomar@thoughtworks.com>
2022-11-07 22:35:09 +0530

    Initial setup

---
+++ src/main/java/com/shopex/user/User.java
@@ -0,0 +1,56 @@
+package com.shopex.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
---
+++ src/main/java/com/shopex/user/UserPrincipleService.java
@@ -0,0 +1,34 @@
+package com.shopex.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserPrincipleService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        return new UserPrinciple(user);
+    }
+
+    private User findByUsername(String username) {
+        try {
+            return userRepository.findByUsername(username).get();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+
+    }
+
+
+}
