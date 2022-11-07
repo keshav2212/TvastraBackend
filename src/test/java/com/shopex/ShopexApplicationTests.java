1d496239f1d0a71c5f155c6525f26ce268008369
Dakshta Tomar <dakshta.tomar@thoughtworks.com>
2022-11-07 21:38:36 +0530

    Initial commit.

---
+++ src/test/java/com/shopex/ShopexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
