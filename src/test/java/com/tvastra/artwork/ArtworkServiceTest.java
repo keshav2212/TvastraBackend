634113b104525a60418487e5d0996794ff270db0
offset-null1 <nirvindak23@gmail.com>
2022-11-22 16:16:17 +0530

    [Dakshta] Add. builder

---
+++ src/test/java/com/tvastra/artwork/ArtworkServiceTest.java
@@ -0,0 +1,76 @@
+package com.tvastra.artwork;
+
+import com.tvastra.Price.Price;
+import com.tvastra.TvastraApplication;
+import com.tvastra.gallery.Gallery;
+import com.tvastra.gallery.GalleryService;
+import com.tvastra.gallery.artwork.Artwork;
+import com.tvastra.gallery.artwork.ArtworkRepository;
+import com.tvastra.gallery.artwork.ArtworkService;
+import com.tvastra.gallery.artwork.model.ArtworkDTO;
+import com.tvastra.gallery.artwork.model.ArtworkInfoDTO;
+import com.tvastra.gallery.category.Category;
+import com.tvastra.gallery.category.CategoryService;
+import com.tvastra.user.User;
+import com.tvastra.user.UserPrincipleService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = TvastraApplication.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class ArtworkServiceTest {
+
+    @InjectMocks
+    ArtworkService artworkService;
+    @Mock
+    ArtworkRepository artworkRepository;
+
+    @Mock
+    UserPrincipleService principleService;
+
+    @Mock
+    GalleryService galleryService;
+
+    @Mock
+    CategoryService categoryService;
+
+    @Test
+    public void shouldSaveArtworkToRepository() {
+        ArtworkDTO artworkDTO = ArtworkDTO.builder()
+                .info(new ArtworkInfoDTO())
+                .user("testUser")
+                .galleryId(1L)
+                .price(new Price())
+                .categoryId(1L)
+                .build();
+        Artwork artwork = Artwork.builder().build();
+        when(principleService.findByUsername(anyString())).thenReturn(new User());
+        when(galleryService.getById(anyLong())).thenReturn(new Gallery());
+        when(categoryService.getById(anyLong())).thenReturn(new Category());
+        when(artworkRepository.save(any(Artwork.class))).thenReturn(artwork);
+
+
+        artworkService.saveArtwork(artworkDTO, "testPath");
+
+        verify(principleService, times(1)).findByUsername(anyString());
+        verify(galleryService, times(1)).getById(anyLong());
+        verify(categoryService, times(1)).getById(anyLong());
+        verify(artworkRepository, times(1)).save(any(Artwork.class));
+
+//        relook
+//        verifyNoInteractions(principleService);
+//        verifyNoInteractions(galleryService);
+//        verifyNoMoreInteractions(categoryService);
+//        verifyNoInteractions(artworkRepository);
+    }
+
+
+
+}
---
+++ src/test/java/com/tvastra/gallery/GalleryServiceTest.java
@@ -0,0 +1,98 @@
+package com.tvastra.gallery;
+
+import com.tvastra.Price.Price;
+import com.tvastra.TvastraApplication;
+import com.tvastra.gallery.artwork.Artwork;
+import com.tvastra.gallery.artwork.ArtworkInfo;
+import com.tvastra.gallery.artwork.ArtworkService;
+import com.tvastra.gallery.category.Category;
+import com.tvastra.user.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(classes = TvastraApplication.class)
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class GalleryServiceTest {
+
+    @InjectMocks
+    GalleryService galleryService;
+
+    @Mock
+    GalleryRepository galleryRepository;
+
+    @Mock
+    ArtworkService artworkService;
+
+    @Test
+    public void shouldSaveGalleryWhenGalleryNameIsGiven() {
+        String inputGalleryName = "Test Gallery";
+        Gallery gallery = Gallery.builder().name(inputGalleryName).build();
+        when(galleryRepository.save(any(Gallery.class))).thenReturn(gallery);
+
+        galleryService.saveGallery(gallery);
+
+        verify(galleryRepository, times(1)).save(any(Gallery.class));
+        verifyNoMoreInteractions(galleryRepository);
+    }
+
+    @Test
+    public void shouldReturnGalleryWhenGalleryIdIsGiven() {
+        Gallery gallery = Gallery.builder().name("testGallery").build();
+        when(galleryRepository.findById(anyLong())).thenReturn(Optional.ofNullable(gallery));
+
+        Gallery actual = galleryService.getById(getRandomLong());
+
+        verify(galleryRepository, times(1)).findById(anyLong());
+        verifyNoMoreInteractions(galleryRepository);
+
+
+    }
+
+    @Test
+    public void shouldGetGalleryArtworksWhenGalleryNameIsGiven() {
+        String testGallery = "Test Gallery";
+        Artwork firstArtwork = Artwork.builder().info(new ArtworkInfo())
+                .addedBy(new User())
+                .gallery(new Gallery())
+                .price(new Price())
+                .artImagePath("/testUploads")
+                .category(new Category())
+                .build();
+        Artwork secondArtwork = Artwork.builder()
+                .build();
+        List<Artwork> artworks = new ArrayList<>();
+        artworks.add(firstArtwork);
+        artworks.add(secondArtwork);
+        when(galleryRepository.findByName(testGallery)).thenReturn(Gallery.builder().id(1L).build());
+        when(artworkService.getGalleryArtworks(anyLong())).thenReturn(artworks);
+
+        List<Artwork> artworkList = galleryService.getArtworks(testGallery);
+
+        assertEquals(artworks, artworkList);
+        verify(galleryRepository, times(1)).findByName(anyString());
+        verify(artworkService, times(1)).getGalleryArtworks(anyLong());
+        verifyNoMoreInteractions(galleryRepository);
+        verifyNoMoreInteractions(artworkService);
+    }
+
+    private Long getRandomLong() {
+        return new Random().longs(1, 10).findFirst().getAsLong();
+    }
+
+}
