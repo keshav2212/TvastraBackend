364a7bc442bc7fadaa2a661a279375b860577143
Dakshta Tomar <69747184+offset-null1@users.noreply.github.com>
2022-11-23 20:45:12 +0530

    Revert "[Dakshta] Fix. conflict"
    This reverts commit e11df779b482823e9235598e1b6a56c1d04fff11.

--- src/main/java/com/tvastra/gallery/category/CategoryController.java
+++ src/main/java/com/tvastra/gallery/category/CategoryController.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/category")
-public class
-CategoryController {
+public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
