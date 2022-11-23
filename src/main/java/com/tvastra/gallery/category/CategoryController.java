580e60dfe4d4b6157ace2e57af6c8bd4bfc5fb1e
offset-null1 <nirvindak23@gmail.com>
2022-11-23 19:43:10 +0530

    [Dakshta] Fix. conflict

--- src/main/java/com/tvastra/gallery/category/CategoryController.java
+++ src/main/java/com/tvastra/gallery/category/CategoryController.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/category")
-public class CategoryController {
+public class
+CategoryController {
 
     @Autowired
     CategoryService categoryService;
